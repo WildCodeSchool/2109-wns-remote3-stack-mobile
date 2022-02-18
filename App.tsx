0f492c0c1e42d02fdc36334cf8f6b5a7e2abf2d3
Thomas Barrial <thomas.barrial@gmail.com>
2022-02-18 09:59:55 +0100

    eslint config ok

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { ApolloProvider } from "@apollo/client";
-import client from "./API/client";
-import Layout from "./screens/layout";
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import client from './API/client';
+import Layout from './screens/layout';
 
 function App() {
-	return (
-		<ApolloProvider client={client}>
-			<Layout />
-		</ApolloProvider>
-	);
+  return (
+    <ApolloProvider client={client}>
+      <Layout />
+    </ApolloProvider>
+  );
 }
 
 export default App;
--- screens/Settings.tsx
+++ screens/Settings.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import { View, Text } from 'react-native';
 
 function Settings() {
-	return (
-		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-			<Text>Stack settings</Text>
-		</View>
-	);
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Stack settings</Text>
+    </View>
+  );
 }
 
 export default Settings;
