2d1961fbc356a6e0e239f791d5f80230ead64036
Manon Trefoux <manon.trefoux@gmail.com>
2022-02-21 14:22:32 +0100

    refactor: change text

--- components/tasks/TaskListView.tsx
+++ components/tasks/TaskListView.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-// import { useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { Text, View } from 'react-native';
 
-// import { GET_ALL_TASKS } from '../../API/queries/taskQueries';
+import { GET_ALL_TASKS } from '../../API/queries/taskQueries';
 // eslint-disable-next-line camelcase
-// import { GetAllTasks_getAllTasks } from '../../API/types/GetAllTasks';
+import { GetAllTasks_getAllTasks } from '../../API/types/GetAllTasks';
 
-// interface IResponse {
-//   // eslint-disable-next-line camelcase
-//   getAllTasks: GetAllTasks_getAllTasks[];
-// }
+interface IResponse {
+  // eslint-disable-next-line camelcase
+  getAllTasks: GetAllTasks_getAllTasks[];
+}
 function TaskList() {
   // FETCH THE TASK LIST
-  //   const { loading, error, data } = useQuery<IResponse>(GET_ALL_TASKS);
+  const { loading, error, data } = useQuery<IResponse>(GET_ALL_TASKS);
 
-  //   if (loading) {
-  //     return <p>...loading</p>;
-  //   }
-  //   if (error || !data) {
-  //     return <p>error</p>;
-  //   }
+  if (loading) {
+    return <Text>...loading </Text>;
+  }
+  if (error || !data) {
+    return <Text> error </Text>;
+  }
   return (
     <View>
       <Text>Tasklist</Text>
