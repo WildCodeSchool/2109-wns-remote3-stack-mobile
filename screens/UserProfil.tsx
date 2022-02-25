f69b45718e86098fbe6c7c32971428e73ebc4993
louisledoux <19322697+louisledoux@users.noreply.github.com>
2022-02-25 09:38:30 +0100

    feat(auth): add logout

--- screens/UserProfil.tsx
+++ screens/UserProfil.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 import tw from 'tailwind-react-native-classnames';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { useMutation } from '@apollo/client';
 import { RootTabParamList } from '../types';
+import { LOGOUT_MUTATION } from '../API/mutation/logout';
+import { useUserFromStore } from '../store/slices/user.slice';
 
 export default function UserProfil() {
   const styles = StyleSheet.create({
@@ -14,6 +17,10 @@ export default function UserProfil() {
       justifyContent: 'center',
     },
   });
+  const { dispatchLogout } = useUserFromStore();
+  const [logoutMutation] = useMutation(LOGOUT_MUTATION, {
+    onCompleted: () => dispatchLogout(),
+  });
 
   type navigationProps = StackNavigationProp<RootTabParamList, 'Homepage'>;
 
@@ -22,6 +29,9 @@ export default function UserProfil() {
     <View style={styles.container}>
       <TouchableOpacity onPress={() => navigation.navigate('Homepage')}>
         <Text style={tw`text-white mb-10 bg-blue-800 p-5`}>Back home</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => logoutMutation()}>
+        <Text style={tw`text-white mb-10 bg-red-800 p-5`}>Se déconnecter</Text>
       </TouchableOpacity>
       <Text style={tw`text-white font-bold text-2xl`}>UserProfil</Text>
     </View>
