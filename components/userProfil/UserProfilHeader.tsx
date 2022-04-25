4a41efe1df23555aeec87bf93ccbf960cc09fa59
Thomas Barrial <thomas.barrial@gmail.com>
2022-04-25 16:35:59 +0200

    refacto userProfil.tsx file

---
+++ components/userProfil/UserProfilHeader.tsx
@@ -0,0 +1,71 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { AntDesign } from '@expo/vector-icons';
+import { useMutation } from '@apollo/client';
+import * as SecureStore from 'expo-secure-store';
+import tw from 'tailwind-react-native-classnames';
+import { useUserFromStore } from '../../store/slices/user.slice';
+import { LOGOUT_MUTATION } from '../../API/mutation/logout';
+
+interface IProps {
+  userId: string;
+}
+
+const styles = StyleSheet.create({
+  backgroundButton: {
+    backgroundColor: '#8790E0',
+  },
+});
+
+export default function UserProfilHeader({ userId }: IProps) {
+  const { user: userFromStore } = useUserFromStore();
+  const navigation = useNavigation();
+  const { dispatchLogout } = useUserFromStore();
+  const [logoutMutation] = useMutation(LOGOUT_MUTATION, {
+    onCompleted: () => {
+      SecureStore.deleteItemAsync('token');
+      dispatchLogout();
+    },
+  });
+  return (
+    <View style={tw`flex flex-row w-full justify-between  items-center`}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Root')}
+        style={tw`h-16 flex flex-row items-center mr-1`}
+      >
+        <AntDesign name="left" size={32} color="#8790E0" style={tw``} />
+      </TouchableOpacity>
+      {userId === userFromStore.id && (
+        <View style={tw`mt-5`}>
+          <TouchableOpacity
+            style={[
+              tw`flex flex-row items-center px-8 py-2 rounded-md`,
+              styles.backgroundButton,
+            ]}
+            onPress={() => navigation.navigate('Settings')}
+          >
+            <Text style={tw`text-white text-sm mr-1 font-bold`}>
+              Edit profil
+            </Text>
+            <AntDesign name="edit" size={16} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              tw`flex flex-row items-center justify-end  mt-2 rounded-md`,
+            ]}
+            onPress={() => logoutMutation()}
+          >
+            <Text style={tw`text-white text-sm font-bold`}>LogOut</Text>
+            <AntDesign
+              style={[tw`ml-2`]}
+              name="logout"
+              size={12}
+              color="white"
+            />
+          </TouchableOpacity>
+        </View>
+      )}
+    </View>
+  );
+}
--- screens/UserProfil.tsx
+++ screens/UserProfil.tsx
@@ -1,16 +1,14 @@
-import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import React, { useState } from 'react';
 import tw from 'tailwind-react-native-classnames';
-import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
-import { useMutation, useQuery } from '@apollo/client';
-import * as SecureStore from 'expo-secure-store';
-import { AntDesign } from '@expo/vector-icons';
-import { LOGOUT_MUTATION } from '../API/mutation/logout';
-import { useUserFromStore } from '../store/slices/user.slice';
+import { RouteProp, useRoute } from '@react-navigation/native';
+import { useQuery } from '@apollo/client';
 import UserProfilNavigation from '../components/userProfil/UserProfilNavigation';
 import { GetUserByID } from '../API/types/GetUserByID';
 import { GET_USER_BY_ID } from '../API/queries/userQueries';
 import Loader from '../components/Loader';
+import UserProfilHeader from '../components/userProfil/UserProfilHeader';
+import ProfilPicture from '../components/userProfil/ProfilPicture';
 
 const styles = StyleSheet.create({
   container: {
@@ -20,25 +18,6 @@ const styles = StyleSheet.create({
     paddingTop: 40,
     paddingHorizontal: 20,
   },
-  image: {
-    height: 120,
-    width: 120,
-    borderRadius: 100,
-    borderWidth: 2,
-    borderColor: '#8790E0',
-  },
-  textColor: {
-    color: '#8790E0',
-  },
-  backgroundColor: {
-    backgroundColor: '#8790E0',
-  },
-  defaultAvatar: {
-    backgroundColor: '#8790E0',
-  },
-  letter: {
-    fontSize: 70,
-  },
 });
 
 type RootStackParam = {
@@ -46,18 +25,9 @@ type RootStackParam = {
 };
 
 export default function UserProfil() {
-  const { user: userFromStore } = useUserFromStore();
-  const navigation = useNavigation();
   const route = useRoute<RouteProp<RootStackParam>>();
   const { id } = route.params;
   const [nav, setNav] = useState('projects');
-  const { dispatchLogout } = useUserFromStore();
-  const [logoutMutation] = useMutation(LOGOUT_MUTATION, {
-    onCompleted: () => {
-      SecureStore.deleteItemAsync('token');
-      dispatchLogout();
-    },
-  });
 
   const {
     data: user,
@@ -66,8 +36,6 @@ export default function UserProfil() {
   } = useQuery<GetUserByID>(GET_USER_BY_ID, {
     variables: { getUserByIdId: id },
   });
-
-  const letter = user?.getUserByID.firstName?.split('')[0];
 
   if (loading) {
     return <Loader />;
@@ -78,63 +46,13 @@ export default function UserProfil() {
 
   return (
     <View style={styles.container}>
-      <View style={tw`flex flex-row w-full justify-between  items-center`}>
-        <TouchableOpacity
-          onPress={() => navigation.navigate('Root')}
-          style={tw`h-16 flex flex-row items-center mr-1`}
-        >
-          <AntDesign name="left" size={32} color="#8790E0" style={tw``} />
-        </TouchableOpacity>
-        {user.getUserByID.id === userFromStore.id && (
-          <View style={tw`mt-5`}>
-            <TouchableOpacity
-              style={[
-                tw`flex flex-row items-center px-8 py-2 rounded-md`,
-                styles.backgroundColor,
-              ]}
-              onPress={() => navigation.navigate('Settings')}
-            >
-              <Text style={tw`text-white text-sm mr-1 font-bold`}>
-                Edit profil
-              </Text>
-              <AntDesign name="edit" size={16} color="white" />
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={[
-                tw`flex flex-row items-center justify-end  mt-2 rounded-md`,
-              ]}
-              onPress={() => logoutMutation()}
-            >
-              <Text style={tw`text-white text-sm font-bold`}>LogOut</Text>
-              <AntDesign
-                style={[tw`ml-2`]}
-                name="logout"
-                size={12}
-                color="white"
-              />
-            </TouchableOpacity>
-          </View>
-        )}
-      </View>
+      <UserProfilHeader userId={user.getUserByID.id} />
 
       <View style={tw`w-full flex flex-col items-center mt-2`}>
-        {user.getUserByID.avatar ? (
-          <Image
-            style={styles.image}
-            source={{
-              uri: user.getUserByID.avatar,
-            }}
-          />
-        ) : (
-          <View
-            style={[
-              tw`h-32 w-32  rounded-full flex items-center justify-center`,
-              styles.defaultAvatar,
-            ]}
-          >
-            <Text style={[tw`text-white`, styles.letter]}>{letter}</Text>
-          </View>
-        )}
+        <ProfilPicture
+          userFirstName={user.getUserByID.firstName}
+          userAvatarUrl={user.getUserByID.avatar}
+        />
 
         <View style={tw`text-white flex flex-row mt-5`}>
           <Text style={tw`text-white font-bold text-xl mr-1`}>
